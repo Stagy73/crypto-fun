a8581e19f4c288731ddb43c00271a0f266d2c64f
Nils <nils.mehlhorn.fr@gmail.com>
2023-09-08 15:26:24 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/header";
 import PriceChange from "./components/PriceChange";
+import CryptocurrencyDisclaimer from "./components/paragraphe";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* <Header /> */}
       {/* <CryptoPrice /> */}
       <PriceChange />
+      <CryptocurrencyDisclaimer />
     </>
   );
 }
---
+++ src/components/paragraphe.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./list.css";
+
+function CryptocurrencyDisclaimer() {
+  return (
+    <div className="paragraphe">
+      <h1>Disclaimer: Risks of Cryptocurrency Trading</h1>
+      <p>
+        Cryptocurrency trading carries a high level of risk and may not be
+        suitable for all investors. The prices of cryptocurrencies are extremely
+        volatile and can fluctuate rapidly. Before engaging in cryptocurrency
+        trading, please consider the following risks:
+      </p>
+      <ul className="ulpara">
+        <li className="ulpara">
+          <strong>Price Volatility:</strong> Cryptocurrency prices can change
+          significantly in a short period. You may experience both substantial
+          gains and losses.
+        </li>
+        <li className="ulpara">
+          <strong>Lack of Regulation:</strong> Cryptocurrencies are not
+          regulated by any central authority or government. This means there may
+          be limited investor protection.
+        </li>
+        <li className="ulpara">
+          <strong>Security Risks:</strong> Crypto wallets and exchanges can be
+          vulnerable to hacking and security breaches. It's essential to use
+          secure platforms and safeguard your private keys.
+        </li>
+        <li className="ulpara">
+          <strong>Market Manipulation:</strong> Cryptocurrency markets can be
+          susceptible to manipulation, pump-and-dump schemes, and fraudulent
+          activities.
+        </li>
+        <li className="ulpara">
+          <strong>Loss of Funds:</strong> There's a risk of losing your entire
+          investment in cryptocurrency trading.
+        </li>
+      </ul>
+      <p>
+        It's crucial to conduct thorough research, understand the risks, and
+        only invest what you can afford to lose. Cryptocurrency trading
+        decisions should be made responsibly, and you should seek advice from
+        financial professionals if needed.
+      </p>
+      <p>
+        <strong>Disclaimer:</strong> This website provides information about
+        cryptocurrencies for educational purposes only. It does not offer
+        financial advice or endorse any specific investment. Users of this
+        website are solely responsible for their cryptocurrency trading
+        decisions, and the website owners and creators disclaim any liability
+        for financial losses.
+      </p>
+    </div>
+  );
+}
+
+export default CryptocurrencyDisclaimer;
