e78cf62d5d61ddf3c8f5b694bf827583d9464e5a
Nils <nils.mehlhorn.fr@gmail.com>
2023-09-11 16:04:20 +0200

    background image fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 
 import PriceChange from "./components/PriceChange";
 import CryptocurrencyDisclaimer from "./components/paragraphe";
-
+import Header from "./components/header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <PriceChange />
       <CryptocurrencyDisclaimer />
       <Footer />
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import header from "../assets/header.jpg";
 import "./Header.css";
 
 function Header() {
   return (
     <header>
-      <img className="header" src={header} alt="Header Image" />
+      <nav className="nav">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        {/* Add more navigation links as needed */}
+      </nav>
     </header>
   );
 }
